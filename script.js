9490d122da1c234f22878b9e0ceab07f020e4c19
kovox <rhkrgyrjsl@naver.com>
2022-11-05 19:27:31 +0900

    22-11-05

--- script.js
+++ script.js
@@ -18,9 +18,61 @@ console.log(document.getElementById("toGrab").autofocus); // will print true (du
 toGrab.innerText = "got you!"; // html tags can be modified with JS. (with tag's id)
 // all tags in HTML is considered an object in JS
 
-
 const hellos = document.getElementsByClassName("hi"); // getElementsByClassName returns an array (object array)
 
 console.log(hellos);
 
 console.log(document.getElementsByTagName("h1"));
+
+const querysel = document.querySelector(".hello h1"); // querySelector allows search an element by css style notation (CSS selector)
+// querySelector will get h1 element in class hello
+// querySelector will return 'an' element. if there's many conforming element, it will return the first one.
+// querySelectorAll will return array of conforming elements.
+
+console.log(querysel);
+
+
+// events (listening)
+
+const color = document.querySelector("div.colorModi h1");
+
+console.dir(color);
+
+color.style.color = "blue";
+
+color.addEventListener("click", handleColorClick); // listens event, end the event happens, call the function(2nd argument is name of function to run)
+// dont type () when call addEventListener. if type (), it calls function 
+color.addEventListener("mouseenter", handleColorMouseEnter);
+
+color.onmouseleave = handleColorMouseLeave; // assigning function to event can be done like this.
+
+// and event listener can be removed by calling removeEventListener
+
+function handleColorClick()
+{
+    console.log("colorModi line was clicked.. now its turning red");
+    color.textContent = "now im red"
+    color.style.color = "red";
+}
+
+function handleColorMouseEnter()
+{
+    console.log("mouse is touching colorModi");
+    color.textContent = "something is on me..";
+    color.style.color = "green"
+}
+
+function handleColorMouseLeave()
+{
+    console.log("mouse leaved.");
+    color.textContent = "its off";
+    color.style.color = "blue";
+}
+
+window.addEventListener("resize", handleWindowResize); // window is an object (like document). 
+
+function handleWindowResize()
+{
+    document.body.style.backgroundColor = "tomato";
+    console.log("window resizing")
+}
